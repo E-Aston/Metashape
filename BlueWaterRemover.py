74f28bb9a1503f69d4acb60b11d80ff2239ff6f5
E-Aston <80836421+E-Aston@users.noreply.github.com>
2021-04-02 12:24:36 +0100

    Add files via upload

---
+++ BlueWaterRemover.py
@@ -0,0 +1,17 @@
+import Metashape
+
+
+doc = Metashape.app.document
+chunk = Metashape.app.document.chunk
+
+dense_cloud = chunk.dense_cloud
+
+
+dense_cloud.selectPointsByColor(color=[85,170,255], tolerance=50, channels='RGB')
+dense_cloud.removeSelectedPoints()
+dense_cloud.selectPointsByColor(color=[151,224,255], tolerance=50, channels='RGB')
+dense_cloud.removeSelectedPoints()
+dense_cloud.selectPointsByColor(color=[117,168,193], tolerance=50, channels='RGB')
+dense_cloud.removeSelectedPoints()
+dense_cloud.selectPointsByColor(color=[117,184,204], tolerance=25, channels='RGB')
+dense_cloud.removeSelectedPoints()
